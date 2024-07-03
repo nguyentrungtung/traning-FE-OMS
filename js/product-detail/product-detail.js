6a69cbd00b213d2342ffdd41233753ee2229c22f
Truy Van <Truyvanlangkhach2512@gmail.com>
2024-07-03 17:26:00 +0700

    Built Product Detail and Origin Finder completely for PC and Mobile Screen and OTW to build Shop Profile webpage - 03/07

--- js/product-detail/product-detail.js
+++ js/product-detail/product-detail.js
@@ -12,3 +12,42 @@ document.getElementById('button-plus').addEventListener('click', function () {
     var value = parseInt(input.value, 10);
     input.value = value + 1;
 });
+
+// img choose size
+
+$(document).ready(function() {
+    $('#mainImg').elevateZoom({
+        zoomType: "window",
+        cursor: "zoom-in",
+        zoomWindowFadeIn: 500,
+        zoomWindowFadeOut: 750,
+        zoomWindowWidth: 400,   
+        zoomWindowHeight: 300,
+        borderSize: 1,
+        borderColour: "#29AB56 !important;",
+        zoomWindowOffetx: 0 //margin left of zoomed element
+    });
+});
+function changeZoomImg(source, target){
+    console.log(document.getElementById(target))
+    var imgTag = source.querySelector('img')
+    var imgSrc = imgTag.src //Get Attribute src
+    imgSrc = imgSrc.split('/') //get all folders that contains img file
+    var name = imgSrc.pop() //get img file name
+    imgSrc = imgSrc.join('/') + '/zoom-' + name; //Change file name with 'zoom' for zooming purpose and merge to the link
+    var targetID = '#' + target;
+    $('.zoomContainer').remove();
+    $(targetID).removeData('elevateZoom');
+    document.getElementById(target).setAttribute('data-zoom-image', imgSrc)
+    $(targetID).data('zoom-image', imgSrc).elevateZoom({
+        zoomType: "window",
+        cursor: "zoom-in",
+        zoomWindowFadeIn: 500,
+        zoomWindowFadeOut: 750,
+        zoomWindowWidth: 400,   
+        zoomWindowHeight: 300,
+        borderSize: 1,
+        borderColour: "#29AB56 !important;",
+        zoomWindowOffetx: 0 //margin left of zoomed element
+    });
+}
