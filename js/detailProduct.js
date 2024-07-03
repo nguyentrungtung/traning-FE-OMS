1a6542f1f8865957265ceb885f26e0f80868db15
tuananh141101 <83608711+tuananh141101@users.noreply.github.com>
2024-07-03 17:23:50 +0700

    Ommanilife 03072024

--- js/detailProduct.js
+++ js/detailProduct.js
@@ -1,8 +1,8 @@
+//*Toggle To See More Detail Product On Mobile Device
 const toggleSeeMoreDetailTitle = document.getElementById(
   "seeMoreDetailTitleProduct"
 );
 const detailTitleProduct = document.getElementById("detail-product");
-console.log("check =", detailTitleProduct);
 
 toggleSeeMoreDetailTitle.addEventListener("click", () => {
   detailTitleProduct.classList.toggle("toggleSeeMore");
